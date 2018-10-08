379150143943f46748d5a60e79fa4583bc0c6c58
valstad <firsov.k@gmail.com>
2018-10-08 14:07:20 +0400

    start1

--- myapp/routes.py
+++ myapp/routes.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*- 
 from flask import render_template
 from myapp import app
+from myapp.forms import LoginForm
 
 @app.route('/')
 @app.route('/index')
@@ -23,3 +24,8 @@ def index():
     ]
     
     return render_template('index.html', title='Home', user=user, posts=posts)
+
+@app.route('/login')
+def login():
+    form = LoginForm()
+    return render_template('login.html', title='Sign In', form=form)
