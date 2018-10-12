fb50c5ca22f857b9c2268879c94da63123ac30a6
valstad <firsov.k@gmail.com>
2018-10-12 16:31:10 +0400

    part X-begin

--- myapp/__init__.py
+++ myapp/__init__.py
@@ -5,10 +5,12 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_login import LoginManager
+from flask_mail import Mail
 from config import Config
 
 app = Flask(__name__)
 app.config.from_object(Config)
+mail=Mail(app)
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 login = LoginManager(app)
